8e8e8d03d19a76a04ca64915d0aed32e267d8efe
ShresthRaj <shresthraj77@gmail.com>
2023-06-15 01:51:38 +0530

    added button

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-fetch('https://icanhazdadjoke.com/slack')
-    .then(data => data.json())
-    .then(jokeData => {
-        const jokeText = jokeData.attachments[0].text;
-        const jokeElement = document.getElementById('jokeElement');
 
-        jokeElement.innerHTML = jokeText;
-    })
+async function getJoke() {
+  const response = await fetch("https://icanhazdadjoke.com/slack");
+  const joke = await response.json();
+  const jokeText = joke.attachments[0].text;
+  const jokeElement = document.getElementById("jokeElement");
+  jokeElement.innerHTML = jokeText;
+}
+
+getJoke();
+
+document.getElementById("btn").addEventListener('click',()=>{
+  getJoke();
+})
